b871f8e2f5852581a8207d9f80f45a68a7099347
fabian <djregenbogen12@icloud.com>
2022-12-03 14:59:15 +0100

    Update

--- 03 Data Clustering/04_first_pytorch_prototype.py
+++ 03 Data Clustering/04_first_pytorch_prototype.py
@@ -91,7 +91,7 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
-epochs = 100
+epochs = 10
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
