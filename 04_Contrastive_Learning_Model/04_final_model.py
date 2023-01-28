a2e05601e22b64ced8c3b7a2e9cf8b310b6f7e04
fabian <djregenbogen12@icloud.com>
2023-01-28 11:45:40 +0100

    HuggingFace Dataloader and more

--- 04_Contrastive_Learning_Model/04_final_model.py
+++ 04_Contrastive_Learning_Model/04_final_model.py
@@ -1,99 +1,212 @@
+import random
 import wandb
 import torch
-import pandas as pd
-import random
-from torch.utils.data import DataLoader
+from torch import nn
 import pytorch_lightning as L
+from datasets.dataset_dict import DatasetDict
+from transformers import AutoTokenizer, AutoModel, BertConfig
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
 
 import sys
 sys.path.append('.')
-from util.style_model import StyleModel
+from util.contrastive_pairs import build_contrastive_pairs_data_dict
 
 # Server GPU
-# wandb.config = {
-#   "batch_size": 512,
-#   "learning_rate": 1e-3,
-#   "max_length": 20,
-#   "epochs": 15,
-#   "num_workers": 48,
-#   "precision": 16,
-#   "accelerator": 'gpu',
-#   "devices": 1,
-#   "subset_size": 500000
-# }
-
-# Local MPS
 wandb.config = {
-  "batch_size": 64,
+  "batch_size": 256,
   "learning_rate": 1e-3,
   "max_length": 20,
-  "epochs": 10,
-  "num_workers": 8,
+  "epochs": 25,
   "precision": 16,
-  "accelerator": 'cpu',
+  "accelerator": 'gpu',
   "devices": 1,
-  "subset_size": 1000
+  "train_subset_size": 70000,
+  "validate_subset_size": 15000
 }
+
+# Local MPS
+# wandb.config = {
+#   "batch_size": 32,
+#   "learning_rate": 1e-3,
+#   "max_length": 20,
+#   "epochs": 10,
+#   "precision": 16,
+#   "accelerator": 'mps',
+#   "devices": 1,
+#   "train_subset_size": 65,
+#   "validate_subset_size": 35
+# }
 
 wandb.init(project="contrastive_model", entity="commit_message_evaluation", config = wandb.config)
 
+def load_data():
+    train = build_contrastive_pairs_data_dict('data/04a_Train_Set.pkl', 1) # , 369)
+    validate = build_contrastive_pairs_data_dict('data/04b_Validate_Set.pkl', 1) #, 650)
+    test = build_contrastive_pairs_data_dict('data/04c_Test_Set.pkl', 1) # 647)
 
-### Load Data
-# Build dataloader with saved files
+    d = {
+        'train': train,
+        'validate': validate,
+        'test': test
+    }
 
-# Pickle really slow
+    dataloader = DatasetDict(d)
+    dataloader.set_format(type='pytorch')
+    print("Tokenization (Runs 3x)")
+    dataloader = dataloader.map(tokenize_function)
+    return dataloader
 
-# with open('data/06a_Contrastive_Train_Pairs.pkl', 'rb') as f:
-#     training_pairs = pickle.load(f)
-# with open('data/06b_Contrastive_Validate_Pairs.pkl', 'rb') as f:
-#    validate_pairs = pickle.load(f)
-# with open('data/06c_Contrastive_Test_Pairs.pkl', 'rb') as f:
-#     testing_pairs = pickle.load(f)
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
-# Build Pairs by the function here
+def tokenize_function(examples):
+    all_features = {}
+    features_1 = tokenizer(examples['messages_1'], padding='max_length', truncation=True, return_tensors='pt', max_length=wandb.config['max_length'])
+    features_2 = tokenizer(examples['messages_2'], padding='max_length', truncation=True, return_tensors='pt', max_length=wandb.config['max_length'])
+    all_features['input_ids_1']      = features_1['input_ids']
+    all_features['token_type_ids_1'] = features_1['token_type_ids']
+    all_features['attention_mask_1'] = features_1['attention_mask']
+    all_features['input_ids_2']      = features_2['input_ids']
+    all_features['token_type_ids_2'] = features_2['token_type_ids']
+    all_features['attention_mask_2'] = features_2['attention_mask']
+    return all_features
 
-import sys
-sys.path.append('.')
-from util.contrastive_pairs import build_contrastive_pairs
+# Mean Pooling - Take attention mask into account for correct averaging
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] # First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-# training_pairs = build_contrastive_pairs('data/04a_Train_Set.pkl', 369)
-# testing_pairs = build_contrastive_pairs('data/04c_Test_Set.pkl', 647)
 
-# Testing training pairs
+class SBERT(L.LightningModule):
+    def __init__(self, frozen = False, pretrained_weights = True):
+        super().__init__()
+        if pretrained_weights:
+            self.sbert = AutoModel.from_pretrained(MODEL)
+        else:
+            config = BertConfig.from_pretrained(MODEL)
+            self.sbert = AutoModel.from_config(config)
+            for param in self.sbert.parameters():
+                param.requires_grad = True
+        if frozen:
+            for param in self.sbert.parameters():
+                param.requires_grad = False
 
-train_data = pd.read_pickle('data/04a_Train_Set.pkl')
-validate_data = pd.read_pickle('data/04b_Validate_Set.pkl')
-test_data = pd.read_pickle('data/04c_Test_Set.pkl')
+    def forward(self, encoding):
+        embeddings = self.sbert(**encoding)
+        sentence_embeddings = mean_pooling(embeddings, encoding['attention_mask'])
+        # Normalize embeddings
+        sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
+        return sentence_embeddings
 
-training_pairs = []
-testing_pairs = []
+loss_fn = nn.CosineEmbeddingLoss()
 
-for i, group in enumerate(train_data.groupby("author_email")):
-    pair = []
-    for i, message in enumerate(group[1]['message']):
-        pair.append(message)
-        if i % 2 == 1:
-            pair.append(1 if random.choice([True, False]) else -1)
-            training_pairs.append(pair)
-            pair = []
+class StyleModel(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.sbert_m_s = SBERT(pretrained_weights=False)
+        self.sbert_m   = SBERT(frozen=True)
 
-for i, group in enumerate(test_data.groupby("author_email")):
-    pair = []
-    for i, message in enumerate(group[1]['message']):
-        pair.append(message)
-        if i % 2 == 1:
-            pair.append(1 if random.choice([True, False]) else -1)
-            testing_pairs.append(pair)
-            pair = []
+    def forward(self, encoding):
+        embeddings_m_s = self.sbert_m_s(encoding)
+        embeddings_m = self.sbert_m(encoding)
+        # maybe use a feed-forward layer here instead
+        embeddings_s = embeddings_m_s - embeddings_m
+        return embeddings_s
 
-# Do this when taking a subset to ensure that shuffling happens before taking subsets
-random.shuffle(training_pairs)
-random.shuffle(testing_pairs)
 
-train_dataloader = DataLoader(training_pairs[:wandb.config['subset_size']], wandb.config['batch_size'], shuffle=True, drop_last=True, num_workers=wandb.config['num_workers'])
-test_dataloader = DataLoader(testing_pairs[:wandb.config['subset_size']], wandb.config['batch_size'], shuffle=False, drop_last=True, num_workers=wandb.config['num_workers'])
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.sbert_m_s.parameters(), lr=wandb.config['learning_rate'])
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        encoding_1 = {}
+        encoding_1['input_ids'] = batch['input_ids_1'].squeeze()
+        encoding_1['token_type_ids'] = batch['token_type_ids_1'].squeeze()
+        encoding_1['attention_mask'] = batch['attention_mask_1'].squeeze()
+
+        encoding_2 = {}
+        encoding_2['input_ids'] = batch['input_ids_2'].squeeze()
+        encoding_2['token_type_ids'] = batch['token_type_ids_2'].squeeze()
+        encoding_2['attention_mask'] = batch['attention_mask_2'].squeeze()
+
+        # Compute prediction error
+        X1_s = self(encoding_1)
+        X2_s = self(encoding_2)
+
+        # Compute Loss
+        loss = loss_fn(X1_s, X2_s, batch['target'])
+        
+        # Log Loss to WandB
+        wandb.log({"train_loss": loss})
+
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        encoding_1 = {}
+        encoding_1['input_ids'] = batch['input_ids_1'].squeeze()
+        encoding_1['token_type_ids'] = batch['token_type_ids_1'].squeeze()
+        encoding_1['attention_mask'] = batch['attention_mask_1'].squeeze()
+
+        encoding_2 = {}
+        encoding_2['input_ids'] = batch['input_ids_2'].squeeze()
+        encoding_2['token_type_ids'] = batch['token_type_ids_2'].squeeze()
+        encoding_2['attention_mask'] = batch['attention_mask_2'].squeeze()
+
+        # Compute prediction error
+        X1_s = self(encoding_1)
+        X2_s = self(encoding_2)
+
+        # Compute Loss
+        loss = loss_fn(X1_s, X2_s, batch['target'])
+
+        # Log Loss to WandB
+        wandb.log({"val_loss": loss})
+        self.log('val_loss', loss, batch_size=wandb.config['batch_size'])
+
+
+# class ContrastivePairsData(L.LightningDataModule):
+#     def __init__(self, batch_size):
+#         super().__init__()
+#         self.batch_size = batch_size
+
+#     def prepare_data(self):
+#         load_data()
+
+#     def train_dataloader(self):
+#         return torch.utils.data.DataLoader(
+#             dataset=data['train'][:wandb.config['subset_size']],
+#             batch_size=wandb.config['batch_size'],
+#             shuffle=True
+#         )
 
 if __name__ == '__main__':
+    data = load_data()
+    
+    train_subset_indices = random.choices(list(range(0, len(data['train']))), k=wandb.config['train_subset_size'])
+    train_subset = torch.utils.data.Subset(data['train'], train_subset_indices)
+
+    validate_subset_indices = random.choices(list(range(0, len(data['validate']))), k=wandb.config['validate_subset_size'])
+    validate_subset = torch.utils.data.Subset(data['validate'], validate_subset_indices)
+    
+    train_dataloader = torch.utils.data.DataLoader(
+        dataset=train_subset,
+        batch_size=wandb.config['batch_size'],
+        shuffle=True,
+        drop_last=True
+    )
+
+    validate_dataloader = torch.utils.data.DataLoader(
+        dataset=validate_subset,
+        batch_size=wandb.config['batch_size']
+        drop_last=True
+    )
+
     model = StyleModel()
     wandb.watch(model)
     trainer = L.Trainer(
@@ -102,5 +215,5 @@ if __name__ == '__main__':
         max_epochs=wandb.config['epochs'],
         precision=wandb.config['precision']
     )
-    trainer.fit(model, train_dataloader, test_dataloader)
-    # torch.save(model.state_dict(), 'model/Style_Model.pt')
+    trainer.fit(model, train_dataloader, validate_dataloader)
+    torch.save(model.state_dict(), 'model/Subset_Style_Model.pt')
--- util/contrastive_pairs.py
+++ util/contrastive_pairs.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from tqdm import tqdm
+from datasets import Dataset
 
 def build_contrastive_pairs(data_path, cut_amount):
     train_data = pd.read_pickle(data_path)
@@ -26,3 +28,46 @@ def build_contrastive_pairs(data_path, cut_amount):
 
     positive_training_pairs.extend(negative_training_pairs)
     return positive_training_pairs
+
+
+
+def build_contrastive_pairs_data_dict(data_path, cut_amount):
+    data = pd.read_pickle(data_path)
+    train_data_groups = data.groupby("author_email")
+
+    positive_count = 0
+    negative_count = 0
+
+    messages_1 = []
+    messages_2 = []
+    target     = []
+
+    print("Setting up Contrastive Pairs (Runs 6x)")
+    for group in tqdm(train_data_groups):
+        for i, message_1 in enumerate(group[1]['message']):
+            for message_2 in group[1]['message'].iloc[i+1:]:
+                messages_1.append(message_1)
+                messages_2.append(message_2)
+                target.append(1)
+                positive_count += 1
+
+    groups_calculated = []
+
+    print("Setting up Contrastive Pairs (Runs 6x)")
+    for group in tqdm(train_data_groups):
+        groups_calculated.append(group[0])
+        negative_groups = [group if group[0] not in groups_calculated else None for group in train_data_groups]
+        negative_groups = list(filter(lambda item: item is not None, negative_groups))
+        for message_1 in group[1]['message'].sample(n=cut_amount):
+            for negative_group in negative_groups:
+                for message_2 in negative_group[1]['message'].sample(n=cut_amount):
+                    messages_1.append(message_1)
+                    messages_2.append(message_2)
+                    target.append(-1)
+                    negative_count += 1
+
+    print("Positive Pairs:")
+    print(positive_count)
+    print("Negative Pairs:")
+    print(negative_count)
+    return Dataset.from_dict({'messages_1': messages_1, 'messages_2': messages_2, 'target': target})
